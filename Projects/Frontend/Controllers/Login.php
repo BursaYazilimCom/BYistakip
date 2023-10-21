c22cb58421c612d605377a0a4618505b10f87d4c
BursaYazilimCom <bursayazilim@hotmail.com>
2023-10-22 00:14:00 +0300

    Bursa Yazılım İş ve Gelir Gider takip Sistemi Ayarlar Bölümü Yapıldı Bölgesel Ayarlar Yapıldı Ödeme Yöntemleri Yapıldı Sipariş Durumları Yönetimi Para Birimleri ---- Otomatik kur güncelleme YEtki Alanları PErsoneller PErsonel Yetkilendirme Şehirler Müşteriler Yönetimi Ürünler ve Ürün Grupları Yönetimi ---Ürünlerin Ödeme Periyodu ayarları Muhasebe Yönetimi ---Gider (Masraf Yönetimi) ---Kasa Desteri Siparişler Sayfası

---
+++ Projects/Frontend/Controllers/Login.php
@@ -0,0 +1,15 @@
+<?php
+namespace Project\Controllers;
+
+class Login extends Controller
+{
+	public function main()
+	{
+		// Your codes...
+	}
+
+	public function login()
+	{
+		// Your codes...
+	}
+}
